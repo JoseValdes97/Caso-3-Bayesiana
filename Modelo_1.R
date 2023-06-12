ad668af08297282d83ebaee8e706047a6ea2961c
Joan Lamprea <jflh10@gmail.com>
2023-06-12 00:25:50 -0500

    Modelos 1 y 2

--- Modelo_1.R
+++ Modelo_1.R
@@ -1,19 +1,16 @@
-sample_bet <- function(X, y, sig2, lambda, p)
-{
-      return(c(mvtnorm::rmvnorm(n = 1, mean = solve(1/sig2*t(X)%*%X+lambda/sig2*diag(1,nrow=p))%*%(1/sig2*t(X)%*%y), sigma = solve(1/sig2*t(X)%*%X+lambda/sig2*diag(1,nrow=p)))))
+sample_bet_1 <- function(X, y, sig2, lambda, p){
+      c(mvtnorm::rmvnorm(n = 1, mean = solve((1/sig2)*t(X)%*%X+(lambda/sig2)*diag(1,nrow=p))%*%(t(X)%*%y)*(1/sig2), sigma = solve((1/sig2)*t(X)%*%X+(lambda/sig2)*diag(1,nrow=p))))
 }
 
-sample_sig2 <- function(X, y, nu0, sig20, lambda, bet, p,n) 
-{
-      return(1/rgamma(n = 1, shape = 0.5*(n + p + nu0), rate = 0.5*(nu0*sig20+lambda*t(bet)%*%bet+t(y-X%*%bet)%*%(y-X%*%bet))))
+sample_sig2_1 <- function(X, y, nu0, sig20, lambda, bet, p, n) {
+      1/rgamma(n = 1, shape = 0.5*(n + p + nu0), rate = 0.5*(nu0*sig20+lambda*t(bet)%*%bet+t(y-X%*%bet)%*%(y-X%*%bet)))
 }
 
-sample_lambda <- function(X, al, bl, sig2, bet, p)
-{
-      return(rgamma(n = 1, shape = 0.5*p+al, rate = 0.5*(1/sig2*t(bet)%*%bet+bl)))
+sample_lambda_1 <- function(X, al, bl, sig2, bet, p){
+      rgamma(n = 1, shape = (0.5*p)+al, rate = ((1/(2*sig2))*t(bet)%*%bet)+bl)
 }
 # Primer Modelo 
-MCMC_1 <- function(X,y,B,verbose = TRUE){
+MCMC_1 <- function(X,y,B){
       # Constantes 
       n      <- nrow(X)
       p      <- ncol(X)
@@ -22,8 +19,10 @@ MCMC_1 <- function(X,y,B,verbose = TRUE){
       al     <- 1
       bl     <- 2
       bet    <- solve(t(X)%*%X)%*%t(X)%*%y
-      sig20  <- sum((y - X%*%bet)^2)/(n-p)
-      lambda <- rgamma(n=1,shape=al,rate=bl)
+      sig20  <- sum(t(y-X%*%bet)%*%(y-X%*%bet))/(n-p)
+      set.seed(2023)
+      lambda <- rgamma(n = 1, shape = al, rate = bl)
+      set.seed(2023)
       sig2   <- 1/rgamma(n = 1, shape = 0.5*(nu0), rate = 0.5*(nu0*sig20))
       # Almacenamiento
       LAMBDA <- SIG2 <- BET <- LP <- NULL
@@ -31,12 +30,12 @@ MCMC_1 <- function(X,y,B,verbose = TRUE){
       # Actualizar
       set.seed(2023)
       for (b in 1:B){
-            sig2   <- sample_sig2(X, y, nu0, sig20, lambda, bet, p,n)
-            lambda <- sample_lambda(X, al, bl, sig2, bet, p)
-            beta   <- sample_bet(X, y, sig2, lambda, p)
+            bet    <- sample_bet_1(X, y, sig2, lambda, p)
+            sig2   <- sample_sig2_1(X, y, nu0, sig20, lambda, bet, p, n)
+            lambda <- sample_lambda_1(X, al, bl, sig2, bet, p)
+            LAMBDA <- rbind(LAMBDA, lambda)
             SIG2   <- rbind(SIG2, sig2)
             BET    <- rbind(BET,  bet)
-            LAMBDA <- rbind(LAMBDA, lambda)
             LP     <- rbind(LP, sum(dnorm(x = y, mean = bet, sd = sqrt(sig2), log = T)))
       # progreso
       
